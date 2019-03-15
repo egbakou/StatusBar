4f277501fd8548397eba7be16be8bf0ea3903814
egbakou <egbakoukodjolaurent@gmail.com>
2019-03-15 10:09:10 +0000

    Manage buttons state after click event in code behind.

--- StatusBar/StatusBar/MainView.xaml.cs
+++ StatusBar/StatusBar/MainView.xaml.cs
@@ -9,16 +9,21 @@ namespace StatusBar
         public MainView()
         {
             InitializeComponent();
+            ShowStatusBarButton.IsEnabled = false;
+        }
+       
+        private void HideStatusBarButton_Clicked(object sender, EventArgs e)
+        {
+            DependencyService.Get<IStatusBar>().HideStatusBar();
+            HideStatusBarButton.IsEnabled = false;
+            ShowStatusBarButton.IsEnabled = true;
         }
 
         private void ShowStatusBarButton_Clicked(object sender, EventArgs e)
         {
-            DependencyService.Get<IStatusBar>().HideStatusBar();
-        }
-
-        private void HideStatusBarButton_Clicked(object sender, EventArgs e)
-        {
             DependencyService.Get<IStatusBar>().ShowStatusBar();
+            HideStatusBarButton.IsEnabled = true;
+            ShowStatusBarButton.IsEnabled = false;
         }
     }
 }
